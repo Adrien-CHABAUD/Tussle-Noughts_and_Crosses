78eab4d13a3506042cb5b62ea42b53067e4079a5
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2024-02-16 17:29:27 +0000

    Initial Commit

---
+++ Tussle/TussleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TussleApp.swift
+//  Tussle
+//
+//  Created by Adrien CHABAUD on 2024-02-16.
+//
+
+import SwiftUI
+
+@main
+struct TussleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
