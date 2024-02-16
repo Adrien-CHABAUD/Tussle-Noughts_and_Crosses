da99b9a8605547a419b4cdd6d25b5e03aa7fe25d
Adrien CHABAUD <59933787+Adrien-CHABAUD@users.noreply.github.com>
2024-02-16 17:35:46 +0000

    Adding assets + New view

---
+++ Tussle/WelcomeView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeView.swift
+//  Tussle
+//
+//  Created by Adrien CHABAUD on 2024-02-16.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeView()
+}
